2d4a81a5cf214af64d72ba8d0ffa4ee08d19a733
Seppe Gadeyne <seppe@fushia.be>
2025-02-13 09:09:07 +0100

    update

--- check-miner.sh
+++ check-miner.sh
@@ -9,8 +9,8 @@ if ! command -v killall &> /dev/null; then
     sudo apt-get update && sudo apt-get install -y psmisc
 fi
 
-# Check if the log file is empty or contains "cu0 0.00"
-if [ ! -s "$LOG_FILE" ] || grep -q "cu0 0.00" "$LOG_FILE"; then
+# Check if the log file is empty or contains "cu0 0.00" after the initial startup lines
+if [ ! -s "$LOG_FILE" ] || tail -n +100 "$LOG_FILE" | sed 's/\x1b\[[0-9;]*m//g' | grep "cu0 0.00"; then
     echo "Miner is not running, hash power is zero, or log file is empty. Restarting miner..."
     killall -9 quai-gpu-miner-nvidia
     $MINER_START_SCRIPT &
