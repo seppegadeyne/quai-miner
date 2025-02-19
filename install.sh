5b916f6b7b3fc4db0f896e676aa35b05ad7cbea0
Seppe Gadeyne <seppe@fushia.be>
2025-02-19 15:46:45 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 sudo apt update 
 sudo apt upgrade -y
-sudo apt install -y nvidia-cuda-toolkit psmisc screen vim cron
+sudo apt install -y psmisc screen vim cron
+#sudo apt install -y nvidia-cuda-toolkit
 
 sudo service cron start
 (crontab -l ; echo "*/5 * * * * $HOME/quai-miner/check-miner.sh") | crontab -
