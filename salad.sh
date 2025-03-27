9cfb0781d8e0cb9443040af773592de70971b257
Seppe Gadeyne <seppe@fushia.be>
2025-03-27 12:35:33 +0100

    Create salad.sh

---
+++ salad.sh
@@ -0,0 +1,10 @@
+cd ~ && \
+export DEBIAN_FRONTEND=noninteractive && \
+apt-get update && \
+ln -fs /usr/share/zoneinfo/Europe/Brussels /etc/localtime && \
+apt-get install -y tzdata git && \
+dpkg-reconfigure -f noninteractive tzdata && \
+git clone https://github.com/seppegadeyne/quai-miner && \
+cd ~/quai-miner && \
+./install.sh && \
+./check.sh
