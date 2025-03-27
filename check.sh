48bdf9a62799bb6a8a2684e4c676553ecc0d8f26
Seppe Gadeyne <seppe@fushia.be>
2025-03-27 13:10:45 +0100

    update

--- check.sh
+++ check.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 LOG_FILE="$HOME/quai-miner/miner.log"
-MINER_START_SCRIPT="$HOME/quai-miner/run-quai-miner.sh"
+MINER_START_SCRIPT="$HOME/quai-miner/run.sh"
 
 # Check if killall command is available, if not install it
 if ! command -v killall &> /dev/null; then
     echo "killall command not found. Installing..."
-    sudo apt-get update && sudo apt-get install -y psmisc
+    apt-get update && apt-get install -y psmisc
 fi
 
 # Check if the log file is empty or contains "cu0 0.00" after the initial startup lines
--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 apt-get update 
 apt-get upgrade -y
 apt-get install -y psmisc screen vim cron
-apt-get install -y nvidia-cuda-toolkit
+#apt-get install -y nvidia-cuda-toolkit
 
 service cron start
-(crontab -l ; echo "*/5 * * * * $HOME/quai-miner/check-miner.sh") | crontab -
+(crontab -l ; echo "*/5 * * * * $HOME/quai-miner/check.sh") | crontab -
