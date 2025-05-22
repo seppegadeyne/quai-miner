b5cf8569180fbf090bcbd86a8ec2dfe43a4eb8f6
root <root@hard-tree-grows-fin-01.datacrunch.io>
2025-05-22 11:43:01 +0000

    update

--- check.sh
+++ check.sh
@@ -3,16 +3,10 @@
 LOG_FILE="$HOME/quai-miner/miner.log"
 MINER_START_SCRIPT="$HOME/quai-miner/run.sh"
 
-# Check if killall command is available, if not install it
-if ! command -v killall &> /dev/null; then
-    echo "killall command not found. Installing..."
-    apt-get update && apt-get install -y psmisc
-fi
-
 # Check if the log file is empty or contains "cu0 0.00" after the initial startup lines
 if [ ! -s "$LOG_FILE" ] || tail -n +100 "$LOG_FILE" | sed 's/\x1b\[[0-9;]*m//g' | grep "cu0 0.00"; then
     echo "Miner is not running, hash power is zero, or log file is empty. Restarting miner..."
-    killall -9 quai-gpu-miner-nvidia-latest 
+    pkill -f quai-gpu-miner 
     $MINER_START_SCRIPT &
 fi
 
--- reboot.sh
+++ reboot.sh
@@ -2,12 +2,5 @@
 
 MINER_START_SCRIPT="$HOME/quai-miner/run.sh"
 
-# Check if killall command is available, if not install it
-if ! command -v killall &> /dev/null; then
-    echo "killall command not found. Installing..."
-    apt-get update && apt-get install -y psmisc
-fi
-
-killall -9 quai-gpu-miner-nvidia-latest
+pkill -f quai-gpu-miner
 $MINER_START_SCRIPT &
-
