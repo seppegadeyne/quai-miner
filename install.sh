7aeda2559d0cf30fd1e9dfa1d201658f5419a57b
root <root@hard-tree-grows-fin-01.datacrunch.io>
2025-05-23 07:00:02 +0000

    update

--- install.sh
+++ install.sh
@@ -10,5 +10,5 @@ systemctl start cron
 
 # Voeg cronjobs toe
 (crontab -l 2>/dev/null || true; echo "*/5 * * * * /bin/bash $HOME/quai-miner/check.sh") | crontab -
-(crontab -l 2>/dev/null || true; echo "0 */4 * * * /bin/bash $HOME/quai-miner/reboot.sh") | crontab -
+(crontab -l 2>/dev/null || true; echo "0 */1 * * * /bin/bash $HOME/quai-miner/reboot.sh") | crontab -
 
